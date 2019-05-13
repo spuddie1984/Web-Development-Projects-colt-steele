edada563f29a6df76d2d0e9e7c552e5be48477e4
spuddie1984 <spuddie1984@hotmail.com>
2019-05-13 10:17:19 +0800

    create app.js and related instructions

---
+++ section 25/app.js
@@ -0,0 +1,18 @@
+// Express Routing Assignment
+
+// 1. Create a brand new Express app from scratch
+// 2. Create a package.json using `npm init` and add express as a dependency
+// 3. In your main app.js file, add 3 different routs:
+
+// Visiting "/" Should print "Hi there, welcome to my assignment!"
+
+// Visting "/speak/pig" Should print "The pig says 'Oink"
+// Visiting "/speak/cow" Should print "The cow says 'Moo'"
+// Visiting "/speak/dog" Should print "The dog says 'Woof Woof!'"
+
+// Visiting "/repeat/hello/3" Should print "hello hello hello"
+// Visiting "/repeat/hello/5" Should print "hello hello hello hello hello"
+// Visiting "/repeat/blah/2" Should print "blah blah"
+
+// If a user visits any other route print : "Sorry, page not found...What are you doing with your life?"
+
