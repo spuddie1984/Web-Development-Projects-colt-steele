a1a503ffd1f90f024990017ead19a9b37ae69ce2
spuddie1984 <spuddie1984@hotmail.com>
2019-07-29 11:19:36 +0800

    Update folder structure add .env file update routes with ejs templates

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Web-Development-Projects
 A collection of Web Development Projects based on the Web Developer Bootcamp by Colt Steele
 https://www.udemy.com/the-web-developer-bootcamp/
+
+
+## Yelp Camp Application
+
+###Basics
+#####1st Commit
+setup basic express structure initialize with npm init and add github details, 
+#####2nd Commit
+Add CampGround Route (lists all campgrounds) and landing page route. make an array of objects (camps) that include the image of the campground and the name. Test to see if the routes work!
+Setup views folder structure and partials like header and footer,add a env file for environment variables.
+#####3rd Commit
--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -1,9 +1,25 @@
-// consider adding an .env file later
+
+// For Environment Variables(to be stored in .env)
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000;
+const PORT = process.env.LOCAL_PORT;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// settings
+app.set('view engine', 'ejs');
 
-app.listen(port, () => console.log(`Listening ${port}!`))
+// Landing Page Route
+app.get('/', (req, res) => {
+    res.render("landing")
+})
+
+// array of campgrounds will use a database later (all fake just for demonstration)
+campgrounds = [{"name": "Pine Hill","image":"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},{"name":"Loggers Rest","image":"https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"},{"name":"Coopers Ledge","image":"https://images.unsplash.com/photo-1546811740-23e671faf31c?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"}]
+
+// CampGrounds Route
+app.get('/campgrounds', (req, res) => {
+    res.render("campgrounds", {campgrounds : campgrounds})
+})
+
+app.listen(PORT, () => console.log(`Listening ${PORT}!`))
