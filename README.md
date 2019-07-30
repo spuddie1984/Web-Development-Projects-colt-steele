b8031005da6ec666a9ba0c4dcafdd55228869ba1
spuddie1984 <spuddie1984@hotmail.com>
2019-07-30 11:23:08 +0800

    Yelp Camp Commit 3 (refer to the readme.md for details)

--- README.md
+++ README.md
@@ -9,6 +9,18 @@ https://www.udemy.com/the-web-developer-bootcamp/
 #####1st Commit
 setup basic express structure initialize with npm init and add github details, 
 #####2nd Commit
-Add CampGround Route (lists all campgrounds) and landing page route. make an array of objects (camps) that include the image of the campground and the name. Test to see if the routes work!
+Add CampGround Route (*lists all campgrounds*) and landing page route. make an array of objects (*camps*) that include the image of the campground and the name. Test to see if the routes work!
 Setup views folder structure and partials like header and footer,add a env file for environment variables.
 #####3rd Commit
+######Routes######
+- Add *campgrounds/new* route 
+This route is a form so that the user can add a new campground (*2 inputs one for name of the campground 1 for the image url for that campground*-**More features will be added later**). This form will be sent to the *campgrounds* post route for processing.
+
+- Add *campgrounds* post route
+This Route will process the form data and add that data to the campgrounds array(*The array will be converted into a database later*).  After processing it will redirect to the get *campgrounds* route
+######Styling Etc######
+- Add Basic Styling using bootstrap (*add bootstrap via cdn or downloaded*).
+- setup header and footer partials. Add them to all the templates.
+- Add link to form on campgrounds template 
+- Setup includes file with folder structure for js and css files
+- add an external style sheet to be used app wide (using app.use(express.static()))
--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -6,8 +6,14 @@ const express = require('express')
 const app = express()
 const PORT = process.env.LOCAL_PORT;
 
-// settings
+///////////// SETTINGS /////////////
 app.set('view engine', 'ejs');
+// body parser for POST requests
+app.use(express.urlencoded({ extended: true }))
+// setup static includes folder
+app.use(express.static( "includes" ))
+
+///////////// ROUTES //////////////
 
 // Landing Page Route
 app.get('/', (req, res) => {
@@ -15,11 +21,30 @@ app.get('/', (req, res) => {
 })
 
 // array of campgrounds will use a database later (all fake just for demonstration)
-campgrounds = [{"name": "Pine Hill","image":"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},{"name":"Loggers Rest","image":"https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"},{"name":"Coopers Ledge","image":"https://images.unsplash.com/photo-1546811740-23e671faf31c?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"}]
+campgrounds = [
+    {"name": "Pine Hill","image":"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},
+    {"name":"Loggers Rest","image":"https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"},
+    {"name":"Coopers Ledge","image":"https://images.unsplash.com/photo-1546811740-23e671faf31c?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"},
+    {"name":"Loggers Rest","image":"https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"},
+    {"name":"Loggers Rest","image":"https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"},
+    {"name":"Loggers Rest","image":"https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"},
+    {"name":"Loggers Rest","image":"https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"}
+];
 
 // CampGrounds Route
 app.get('/campgrounds', (req, res) => {
     res.render("campgrounds", {campgrounds : campgrounds})
+})
+
+// Add new CampGround route
+app.get('/campgrounds/new', (req, res) => {
+    res.render("new-campground")
+})
+
+// Route for newly created campgrounds 
+app.post('/campgrounds/', (req, res) => {
+    campgrounds.push({"name":req.body["campground-name"],"image":req.body.image})
+    res.redirect('campgrounds')
 })
 
 app.listen(PORT, () => console.log(`Listening ${PORT}!`))
